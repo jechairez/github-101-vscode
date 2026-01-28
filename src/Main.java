a46b68b881e3d3222711b0db4cd7c5b6606f48f9
José Eduardo Chairez Veloz <chairez_ed1@hotmail.com>
2026-01-28 07:01:48 -0600

    feat:agrega programa java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, GitHub desde Java");
+
+        int a = 2;
+        int b = 3;
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
